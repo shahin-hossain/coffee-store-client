528d80a7e855ccd548d99ecc76367bd624d13713
shahin hossain <shahinhossain3@hotmail.com>
2023-12-23 18:42:21 +0600

    Client Side router and Connect MongoDB database

---
+++ src/components/UpdateCoffee.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const UpdateCoffee = () => {
+    return (
+        <div>
+            <h3>Update a Coffee</h3>
+        </div>
+    );
+};
+
+export default UpdateCoffee;
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import AddCoffee from './components/AddCoffee.jsx'
+import UpdateCoffee from './components/UpdateCoffee.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />
+  },
+  {
+    path: 'addCoffee',
+    element: <AddCoffee />
+  },
+  {
+    path: 'updateCoffee',
+    element: <UpdateCoffee />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
