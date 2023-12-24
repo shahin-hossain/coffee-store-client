e2ef7589c8b62c8da1bb004d61b92c933a3aa7b9
shahin hossain <shahinhossain3@hotmail.com>
2023-12-24 22:05:15 +0600

    Delete a coffee and fetch coffee for udpate

--- src/components/CoffeeCard.jsx
+++ src/components/CoffeeCard.jsx
@@ -1,8 +1,41 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const CoffeeCard = ({ coffee }) => {
-    console.log(coffee)
-    const { photo, name, quantity, supplier, taste, category, details } = coffee;
+    // console.log(coffee)
+    const { _id, photo, name, quantity, supplier, taste, category, details } = coffee;
+
+    const handleDelete = _id => {
+
+        Swal.fire({
+            title: "Are you sure?",
+            text: "You won't be able to revert this!",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#3085d6",
+            cancelButtonColor: "#d33",
+            confirmButtonText: "Yes, delete it!"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                fetch(`http://localhost:5000/coffee/${_id}`, {
+                    method: 'DELETE'
+                })
+                    .then(res => res.json())
+                    .then(data => {
+                        console.log(data)
+                        if (data.deletedCount > 0) {
+                            Swal.fire({
+                                title: "Deleted!",
+                                text: "Your Coffee has been deleted.",
+                                icon: "success"
+                            });
+                        }
+                    })
+            }
+        });
+        console.log(_id)
+    }
     return (
         <div className="card card-side bg-base-100 shadow-xl mb-10">
             <figure><img src={photo} alt="" className='w-[150px] h-[200px]' /></figure>
@@ -18,8 +51,12 @@ const CoffeeCard = ({ coffee }) => {
                 <div className="card-actions justify-end">
                     <div className="join join-vertical">
                         <button className="btn join-item mb-2 btn-neutral">View</button>
-                        <button className="btn join-item mb-2 btn-neutral">Edit</button>
-                        <button className="btn join-item mb-2 btn-neutral">X</button>
+                        <Link to={`/updateCoffee/${_id}`} className="btn join-item mb-2 btn-neutral" >
+                            <button >Edit</button>
+                        </Link>
+                        <button className="btn join-item mb-2 btn-warning"
+                            onClick={() => handleDelete(_id)}
+                        >X</button>
                     </div>
                 </div>
             </div>
--- src/components/UpdateCoffee.jsx
+++ src/components/UpdateCoffee.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
+import { useLoaderData } from 'react-router-dom';
 
 const UpdateCoffee = () => {
+
+    const coffee = useLoaderData();
+    console.log(coffee)
+
     return (
         <div>
             <h3>Update a Coffee</h3>
