c810ad345ee693ecdd18c75f9008c962a96a8fbb
shahin hossain <shahinhossain3@hotmail.com>
2023-12-24 18:10:00 +0600

    Send Coffee data to the server and store in the mongodb database

--- src/components/AddCoffee.jsx
+++ src/components/AddCoffee.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import Swal from 'sweetalert2'
 
 const AddCoffee = () => {
 
@@ -16,6 +17,26 @@ const AddCoffee = () => {
 
         const newCoffee = { name, quantity, supplier, taste, category, details, photo };
         console.log(newCoffee)
+        //send data to server
+        fetch('http://localhost:5000/coffee', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCoffee)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.insertedId) {
+                    Swal.fire({
+                        title: 'Success!',
+                        text: 'Coffee Added Successfully',
+                        icon: 'success',
+                        confirmButtonText: 'Cool'
+                    })
+                }
+            })
     }
     return (
         <div className='bg-[#F4F3F0] p-24'>
